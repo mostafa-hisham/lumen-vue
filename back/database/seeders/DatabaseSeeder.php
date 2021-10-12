5bdae99d7cf40cb268fa8614e76eb9c6e5c1edda
mostafa <mostafa.hisham.mahmoud@gmail.com>
2021-10-12 17:09:15 +0200

    add seeder

--- back/database/seeders/DatabaseSeeder.php
+++ back/database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,8 +14,19 @@ class DatabaseSeeder extends Seeder
      *
      * @return void
      */
+    /**
+     * Run the database seeders.
+     *
+     * @return void
+     */
     public function run()
     {
-        // $this->call('UsersTableSeeder');
+        for ($i = 0; $i <= 10; $i++) {
+            DB::table('users')->insert([
+                'name' => Str::random(10),
+                'email' => Str::random(10) . '@gmail.com',
+                'password' => Hash::make('password'),
+            ]);
+        }
     }
 }
